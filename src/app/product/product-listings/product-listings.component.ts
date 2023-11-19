e6fb5f9b63c5e90459f8a1bbef34a4cdf7e74fed
mizuki-okuyama <m.okuyama@nec.com>
2023-11-19 14:09:01 +0900

    Initial commit

---
+++ src/app/product/product-listings/product-listings.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { products } from '../../products';
+
+@Component({
+  selector: 'app-product-listings',
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.scss']
+})
+export class ProductListingsComponent {
+  products: any 
+
+  ngOnInit(){
+      this.products = products
+  }
+
+}
---
+++ src/app/product/product.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListingsComponent } from './product-listings/product-listings.component';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product.component';
+
+const routes: Routes = [
+    {
+        path: 'products', component: ProductComponent,
+        children: [
+            { path: '', component: ProductListingsComponent },
+            { path: ':productId', component: ProductDetailComponent }
+        ]
+    }
+
+];
+
+@NgModule({
+    declarations: [
+        ProductComponent,
+        ProductDetailComponent,
+        ProductListingsComponent
+    ],
+    imports: [
+        RouterModule.forChild(routes),
+        CommonModule
+    ],
+    providers: [],
+    bootstrap: []
+})
+export class ProductModule { }
